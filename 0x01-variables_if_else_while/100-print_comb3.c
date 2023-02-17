3b4f08325642688eaef18f2feebf70414438e318
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-17 05:50:32 -0800

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+int main() {
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = i + 1; j < 10; j++) {
+            putchar(i + '0');
+            putchar(',');
+            putchar(' ');
+            putchar(j + '0');
+            putchar(' ');
+        }
+    }
+    return 0;
+}
