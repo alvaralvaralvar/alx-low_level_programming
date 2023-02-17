bfff1fcb452c9ab33295103dc345925e4e8b25fb
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-17 08:07:01 -0800

    include right syntax in 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+
 int main() {
     int i, j;
-    for (i = 0; i < 9; i++) {
-        for (j = i + 1; j < 10; j++) {
+
+    for (i = 0; i <= 8; i++) {
+        for (j = i + 1; j <= 9; j++) {
             putchar(i + '0');
-            putchar(',');
-            putchar(' ');
             putchar(j + '0');
-            putchar(' ');
+            if (i < 8) {
+                putchar(',');
+                putchar(' ');
+            }
         }
     }
+
     return 0;
 }
+
