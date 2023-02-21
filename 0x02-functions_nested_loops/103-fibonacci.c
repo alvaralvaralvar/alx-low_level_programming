6835f115969eed1458546d8f57ec238c14a6e533
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-21 15:55:20 -0800

    liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int i;
+    long int prev = 1, curr = 2, next;
+    long int sum = 2;
+
+    while (curr <= 4000000)
+    {
+        next = prev + curr;
+        if (next % 2 == 0)
+        {
+            sum += next;
+        }
+        prev = curr;
+        curr = next;
+    }
+
+    printf("%ld\n", sum);
+
+    return 0;
+}
