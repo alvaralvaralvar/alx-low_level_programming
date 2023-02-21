0467bbd0b48db6d3cb688f1436d87133c48936c4
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-21 02:55:14 -0800

    credentials store test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - Entry Point
+  *
+  *A program that prints _putchar
+  *
+  *Author: Alvar Wakiagi
+  *
+  *return: Always 0
+  *
+  */
+
+int main(void)
+{
+	(1, "_putchar\n", 7);
+	return (0);
+}
