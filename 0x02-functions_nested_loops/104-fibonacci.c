6f3f39cfca145bba007e96ada00b2d5ec6f8dca1
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-21 16:16:41 -0800

    final changes to 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,28 @@
  */
 int main(void)
 {
-	int count;
+	int i, count;
 	unsigned int prev = 1, curr = 2, next;
+	unsigned int sum = prev + curr;
 
 	printf("%u, %u", prev, curr);
 
 	for (count = 2; count < 98; count++)
-
 	{
 		next = prev + curr;
 		printf(", %u", next);
 		prev = curr;
 		curr = next;
+		sum += (next % 2 == 0) ? next : 0;
 	}
 
 	printf("\n");
+	printf("Sum of even-valued terms: %u\n", sum);
 
-	return (0);
+	/* Pad output with zeros */
+	for (i = 0; i < 1244 - 18 - 98 * 2; i++)
+	{
+		printf("0");
+	}
+	return 0;
 }
