5c76f409da7c011a90e747effa70bc5de1affd84
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-21 16:03:18 -0800

    bundled changes to 103-fibonacci.c and 104-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,22 +7,21 @@
  */
 int main(void)
 {
-    int i;
-    long int prev = 1, curr = 2, next;
-    long int sum = 2;
+	long int prev = 1, curr = 2, next;
+	long int sum = 2;
 
-    while (curr <= 4000000)
-    {
-        next = prev + curr;
-        if (next % 2 == 0)
-        {
-            sum += next;
-        }
-        prev = curr;
-        curr = next;
-    }
+	while (curr <= 4000000)
+	{
+		next = prev + curr;
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+		prev = curr;
+		curr = next;
+	}
 
-    printf("%ld\n", sum);
+	printf("%ld\n", sum);
 
-    return 0;
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, count;
+    unsigned int prev = 1, curr = 2, next;
+
+    printf("%u, %u", prev, curr);
+
+    for (count = 2; count < 98; count++)
+    {
+        next = prev + curr;
+        printf(", %u", next);
+        prev = curr;
+        curr = next;
+    }
+
+    printf("\n");
+
+    return (0);
+}
