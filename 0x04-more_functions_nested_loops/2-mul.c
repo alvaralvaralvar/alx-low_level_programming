089c2d254dd0d023c860d715654fe5afcfb152d6
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-23 09:56:58 -0800

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - Prints the digit
+  * @a: input as an integer
+  * @b: input as an integer
+  * Return: last digit
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
