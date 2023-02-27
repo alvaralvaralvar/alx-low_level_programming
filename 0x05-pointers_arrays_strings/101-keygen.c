a97b447005a48e5be0b9b189bccf6306d56fe31e
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-27 13:23:33 -0800

    a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,21 +6,22 @@
 
 int main(void)
 {
-    // Define an array of characters that can appear in the password
+    /* Define an array of characters that can appear in the password */
     char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
 
-    // Seed the random number generator with the current time
-    srand(time(NULL));
+    /* Seed the random number generator with the current time */
+    srand((unsigned int)time(NULL));
 
-    // Generate a random password
-    char password[PASSWORD_LENGTH + 1]; // Add one for the null terminator
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    /* Generate a random password */
+    char password[PASSWORD_LENGTH + 1]; /* Add one for the null terminator */
+    int i;
+    for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         password[i] = charset[rand() % (sizeof(charset) - 1)];
     }
-    password[PASSWORD_LENGTH] = '\0'; // Add the null terminator at the end
+    password[PASSWORD_LENGTH] = '\0'; /* Add the null terminator at the end */
 
-    // Print the password
+    /* Print the password */
     printf("%s\n", password);
 
     return 0;
