2b2ff8a89f55dde33de3a313bfd9489b693e80f1
Alvar Wakiagi <alvarwakiagi@skiff.com>
2023-02-27 13:17:55 -0800

    crackaworking?

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,27 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10 // Maximum password length
+#define PASSWORD_LENGTH 10
+#define CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]\\|;:'\",.<>/?`~"
 
 int main(void)
 {
-    // Define the valid characters for the password
-    const char* valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
-    int num_valid_chars = strlen(valid_chars);
+    srand(time(NULL)); // Seed the random number generator with the current time
 
-    // Seed the random number generator
-    srand(time(NULL));
-
-    // Generate a random password of random length between 1 and PASSWORD_LENGTH
-    int password_length = rand() % PASSWORD_LENGTH + 1;
     char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
-    for (int i = 0; i < password_length; i++)
-    {
-        password[i] = valid_chars[rand() % num_valid_chars];
-    }
-    password[password_length] = '\0'; // Add the null terminator
+    int i;
 
-    // Print the password
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = CHARACTERS[rand() % (sizeof(CHARACTERS) - 1)];
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Add the null terminator to the end of the string
+
     printf("%s\n", password);
 
     return 0;
